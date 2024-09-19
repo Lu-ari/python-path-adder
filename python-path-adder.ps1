96b95b3f747a86484a94ba002b122706923f170d
Mehmet Akif Özdemir <89011948+Lu-ari@users.noreply.github.com>
2024-09-19 15:19:52 +0300

    Update python-path-adder.ps1

--- python-path-adder.ps1
+++ python-path-adder.ps1
@@ -1,5 +1,13 @@
-# Kullanıcı adını klavyeden al
-$userName = Read-Host "Lütfen kullanıcı adınızı girin (YourUsername)"
+
+# Oturum açmış gerçek kullanıcının adını almak için fonksiyon
+function Get-LoggedOnUser {
+    $userName = (Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty UserName)
+    # Sadece kullanıcı adını almak için domain ya da makine adını ayır
+    return $userName.Split('\')[-1]
+}
+
+# Oturum açmış gerçek kullanıcının adını al
+$RealUsername = Get-LoggedOnUser
 
 # Python sürümünü tespit et
 $pythonVersion = & python --version 2>&1
@@ -9,16 +17,19 @@ $pythonVersion = $pythonVersion -replace 'Python ', '' -replace '\s', ''
 $pythonMajorMinor = $pythonVersion -replace '(\d+)\.(\d+)\.\d+', '$1$2'
 
 # Python dizin yollarını ayarla
-$pythonPath = "C:\Users\$userName\AppData\Local\Programs\Python\Python$pythonMajorMinor\"
-$pythonScriptsPath = "C:\Users\$userName\AppData\Local\Programs\Python\Python$pythonMajorMinor\Scripts\"
+$pythonPath = "C:\Users\$RealUsername\AppData\Local\Programs\Python\Python$pythonMajorMinor\"
+$pythonScriptsPath = "C:\Users\$RealUsername\AppData\Local\Programs\Python\Python$pythonMajorMinor\Scripts\"
 
-# Python yolunu ve Scripts dizinini ortam değişkenlerine ekle
+# Sistem Path ortam değişkenini al (System variables)
 $existingPath = [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine)
+
+# Python yolları mevcut değilse sistem Path'e ekle
 if ($existingPath -notlike "*$pythonPath*" -and $existingPath -notlike "*$pythonScriptsPath*") {
-    [Environment]::SetEnvironmentVariable("Path", $pythonPath + ";" + $pythonScriptsPath + ";" + $existingPath, [EnvironmentVariableTarget]::Machine)
-    Write-Host "Python yolları başarıyla ortam değişkenlerine eklendi:"
+    $newPath = "$pythonPath;$pythonScriptsPath;$existingPath"
+    [Environment]::SetEnvironmentVariable("Path", $newPath, [EnvironmentVariableTarget]::Machine)
+    Write-Host "Python yolları başarıyla sistem Path'ine eklendi:"
     Write-Host $pythonPath
     Write-Host $pythonScriptsPath
 } else {
-    Write-Host "Python yolları zaten ortam değişkenlerinde mevcut."
+    Write-Host "Python yolları zaten sistem Path'inde mevcut."
 }
